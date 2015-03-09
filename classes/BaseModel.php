2a1fa2881e1193fd60499d8aec31d54b09c25f28
Jesse Denardo <denaje@gmail.com>
2015-03-09 12:54:48 -0400

    Domain logic handling

--- classes/BaseModel.php
+++ classes/BaseModel.php
@@ -2,6 +2,49 @@
 
 class BaseModel extends Eloquent
 {
+    /*
+     * Translate any integers stored in any foreignKey fields
+     * into their related models.
+     */
+    // Because events are stupid and they don't work
+    public function save(array $options = [])
+    {
+        if (!$this->beforeSave()) {
+            return false;
+        }
+
+        return parent::save($options);
+    }
+
+    private function beforeSave()
+    {
+        if (isset($this->foreignKeys) && $this->foreignKeys) {
+            $this->loadForeignKeys();
+        }
+
+        return true;
+    }
+
+    private function loadForeignKeys()
+    {
+        foreach ($this->foreignKeys as $key) {
+            $this->loadForeignKey($key);
+        }
+    }
+
+    private function loadForeignKey($key)
+    {
+        if (is_int($this->$key)) {
+            $class = ucfirst($key);
+            $foreign_model = $class::findOrFail($this->$key);
+            $this->$key()->associate($foreign_model);
+            unset($this->attributes[$key]);
+        }
+    }
+
+    /*
+     * Load visiblePivotAttributes onto the model
+     */
     public function toArray()
     {
         $array = parent::toArray();
@@ -55,5 +98,25 @@ class BaseModel extends Eloquent
         $foreignKey = $relatedModel->getForeignKey();
 
         return $relatedModel->find($model->pivot->$foreignKey)->toArray();
+    }
+
+    /*
+     * Delegate business logic to a domain entity
+     */
+    protected function delegate($class)
+    {
+        // Instantiate the class we were given
+        $reflection = new ReflectionClass($class);
+        $entity = $reflection->newInstanceArgs([$this]);
+
+        // Get the calling method's name
+        $callerName = debug_backtrace()[1]['function'];
+
+        // Get the name of this attribute based on the function that called us
+        preg_match('/^get(.*)Attribute$/', $callerName, $matches);
+        $attributeName = lcfirst($matches[1]);
+
+        // Pass this object into the class's attribute method
+        return $entity->$attributeName();
     }
 }
