77b0d4567277188b461835a2d40e47c3c41400a1
Jesse Denardo <denaje@gmail.com>
2015-04-30 10:59:50 -0400

    Make TestCase work from any directory

--- classes/TestCase.php
+++ classes/TestCase.php
@@ -20,7 +20,8 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $base_path = preg_replace('/vendor.*/', '', dirname(__DIR__));
+        $app = require $base_path . '/bootstrap/app.php';
 
         $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
